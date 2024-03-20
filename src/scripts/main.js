7a95812c498f7ff86791d81ecd17f26162f6ccb3
Yuri Gabriel Constantino <yuri.gconstantino@gmail.com>
2024-03-20 19:04:49 -0300

    finalizado

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        let valor = document.getElementById('valor').value
+        
+    })
+})
