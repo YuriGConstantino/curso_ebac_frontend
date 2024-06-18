281f4a1fe46251356b6365e575a5d21945204432
Yuri Gabriel Constantino <yuri.gconstantino@gmail.com>
2024-06-18 19:38:05 -0300

    Tratamendo de erros feito

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-    $("#searchBtn").click(function (){
+    $("#searchBtn").click(function () {
         const username = $('#searchInput').val()
         const endpoint = `https://api.github.com/users/${username}`
 
+        if (username.length == 0) {
+            throw new Error(alert("Digite um nome"))
+        } 
 
-        fetch(endpoint).then(function(answer) {
-            return answer.json()
+
+        fetch(endpoint).then(function (response) {
+            if(response.status === 404) {
+                throw new Error(alert("Nome de usuário incorreta ou não encontrada"))
+            }
+            return response.json()
 
         })
-        .then(function(json){
-            console.log(json)
-            const Perfilavatar = json.avatar_url
-            const Perfilname = json.name
-            const Perfilusename = json.login
-            const Perfilrepositories = json.public_repos
-            const Perfilfollowers = json.followers
-            const Perfilfollowing= json.following
 
-            $('#avatar').attr('src', Perfilavatar)
-            $('#username').text(Perfilname)
-            $('#name').text(Perfilusename)
-            $('#repositoriesNumber').text(Perfilrepositories)
-            $('#followersNumber').text(Perfilfollowers)
-            $('#followingNumber').text(Perfilfollowing)
-            $('#profileLink').attr('href', `https://github.com/${username}`)
-        })
-        
+            .then(function (json) {
+                const Perfilavatar = json.avatar_url
+                const Perfilname = json.name
+                const Perfilusename = json.login
+                const Perfilrepositories = json.public_repos
+                const Perfilfollowers = json.followers
+                const Perfilfollowing = json.following
+
+                $('#avatar').attr('src', Perfilavatar)
+                $('#username').text(Perfilname)
+                $('#name').text(Perfilusename)
+                $('#repositoriesNumber').text(Perfilrepositories)
+                $('#followersNumber').text(Perfilfollowers)
+                $('#followingNumber').text(Perfilfollowing)
+                $('#profileLink').attr('href', `https://github.com/${username}`)
+            })
+            .catch(function(error) {
+                alert("Ocorreu um erro")
+            })
+
     })
 })
